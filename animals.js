b816c1b4cee968680d97cda2b0875273c09ee2ac
thomasgusmano <gusmanothomas1@gmail.com>
2025-12-15 21:10:23 -0300

    .

--- animals.js
+++ animals.js
@@ -1,279 +1,123 @@
+async function fetchAnimals() {
+  let res = await fetch("https://dog.ceo/api/breeds/image/random/11");
+  let data = await res.json();
+
+  let animales = getAnimales(data.message);
+  return animales;
+}
+
+
 function getAnimales(dogImages) {
   return [
     {
-      attributes: {
-        name: "Max",
-        species: "Perro",
-        breed: "Labrador",
-        age: "3 años",
-        size: "Mediano",
-        location: "Avellaneda",
-        healthIssues: "Ninguno",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[0]
-      }
+      name: "Max",
+      breed: "Labrador",
+      age: "3 años",
+      size: "Mediano",
+      location: "Avellaneda",
+      healthIssues: "Ninguno",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[0]
     },
     {
-      attributes: {
-        name: "Rocky",
-        species: "Perro",
-        breed: "Bulldog",
-        age: "10 meses",
-        size: "Grande",
-        location: "Quilmes",
-        healthIssues: "Problemas de articulaciones",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[1]
-      }
+      name: "Rocky",
+      breed: "Bulldog",
+      age: "10 meses",
+      size: "Grande",
+      location: "Quilmes",
+      healthIssues: "Problemas de articulaciones",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[1]
     },
     {
-      attributes: {
-        name: "Buddy",
-        species: "Perro",
-        breed: "Beagle",
-        age: "4 años",
-        size: "Mediano",
-        location: "Berazategui",
-        healthIssues: "Problemas de piel",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[2]
-      }
+      name: "Buddy",
+      breed: "Beagle",
+      age: "4 años",
+      size: "Mediano",
+      location: "Berazategui",
+      healthIssues: "Problemas de piel",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[2]
     },
     {
-      attributes: {
-        name: "Luna",
-        species: "Gato",
-        breed: "Siamés",
-        age: "8 meses",
-        size: "Pequeño",
-        location: "Lanús",
-        healthIssues: "Alergia leve",
-        surgeries: "Vacunada",
-        pictureThumbnailUrl: "https://placecats.com/400/400"
-      }
+      name: "Charlie",
+      breed: "Golden Retriever",
+      age: "2 años",
+      size: "Grande",
+      location: "Avellaneda",
+      healthIssues: "Ninguno",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[3]
     },
     {
-      attributes: {
-        name: "Charlie",
-        species: "Perro",
-        breed: "Golden Retriever",
-        age: "2 años",
-        size: "Grande",
-        location: "Morón",
-        healthIssues: "Ninguno",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[3]
-      }
+      name: "Duke",
+      breed: "Rottweiler",
+      age: "5 años",
+      size: "Grande",
+      location: "Avellaneda",
+      healthIssues: "Problemas dentales",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[4]
     },
     {
-      attributes: {
-        name: "Duke",
-        species: "Perro",
-        breed: "Rottweiler",
-        age: "5 años",
-        size: "Grande",
-        location: "Avellaneda",
-        healthIssues: "Problemas dentales",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[4]
-      }
+      name: "Bella",
+      breed: "Pitbull",
+      age: "3 años",
+      size: "Mediano",
+      location: "Lanús",
+      healthIssues: "Sobrepeso",
+      surgeries: "Castrada",
+      pictureThumbnailUrl: dogImages[5]
     },
     {
-      attributes: {
-        name: "Bella",
-        species: "Perro",
-        breed: "Pitbull",
-        age: "3 años",
-        size: "Mediano",
-        location: "Lanús",
-        healthIssues: "Sobrepeso",
-        surgeries: "Castrada",
-        pictureThumbnailUrl: dogImages[5]
-      }
+      name: "Coco",
+      breed: "Chihuahua",
+      age: "1 año",
+      size: "Pequeño",
+      location: "Quilmes",
+      healthIssues: "Ninguno",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[6]
     },
     {
-      attributes: {
-        name: "Coco",
-        species: "Perro",
-        breed: "Chihuahua",
-        age: "1 año",
-        size: "Pequeño",
-        location: "Quilmes",
-        healthIssues: "Ninguno",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[6]
-      }
+      name: "Toby",
+      breed: "Dachshund",
+      age: "6 años",
+      size: "Pequeño",
+      location: "Lomas de Zamora",
+      healthIssues: "Problemas de movilidad",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[7]
     },
     {
-      attributes: {
-        name: "Toby",
-        species: "Perro",
-        breed: "Dachshund",
-        age: "6 años",
-        size: "Pequeño",
-        location: "Lomas de Zamora",
-        healthIssues: "Problemas de movilidad",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[7]
-      }
+      name: "Jack",
+      breed: "Jack Russell Terrier",
+      age: "2 años",
+      size: "Mediano",
+      location: "Ezeiza",
+      healthIssues: "Ninguno",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[8]
     },
     {
-      attributes: {
-        name: "Jack",
-        species: "Perro",
-        breed: "Jack Russell Terrier",
-        age: "2 años",
-        size: "Mediano",
-        location: "San Fernando",
-        healthIssues: "Ninguno",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[8]
-      }
+      name: "Zoe",
+      breed: "Poodle",
+      age: "4 años",
+      size: "Mediano",
+      location: "Florencio Varela",
+      healthIssues: "Alergia",
+      surgeries: "Castrada",
+      pictureThumbnailUrl: dogImages[9]
     },
     {
-      attributes: {
-        name: "Zoe",
-        species: "Perro",
-        breed: "Poodle",
-        age: "4 años",
-        size: "Mediano",
-        location: "Villa Devoto",
-        healthIssues: "Alergia",
-        surgeries: "Castrada",
-        pictureThumbnailUrl: dogImages[9]
-      }
-    },
-    {
-      attributes: {
-        name: "Milo",
-        species: "Perro",
-        breed: "Cocker Spaniel",
-        age: "7 meses",
-        size: "Mediano",
-        location: "Morón",
-        healthIssues: "Ninguno",
-        surgeries: "Castrado",
-        pictureThumbnailUrl: dogImages[10]
-      }
-    },
-
-    // Gatos
-    {
-      attributes: {
-        name: "Simba",
-        species: "Gato",
-        breed: "Persa",
-        age: "3 años",
-        size: "Mediano",
-        location: "Villa Urquiza",
-        healthIssues: "Problemas respiratorios",
-        surgeries: "Vacunado",
-        pictureThumbnailUrl: "https://placekitten.com/400/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Misty",
-        species: "Gato",
-        breed: "Maine Coon",
-        age: "5 años",
-        size: "Grande",
-        location: "San Telmo",
-        healthIssues: "Ninguno",
-        surgeries: "Vacunada",
-        pictureThumbnailUrl: "https://placekitten.com/401/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Oliver",
-        species: "Gato",
-        breed: "Bengalí",
-        age: "2 años",
-        size: "Mediano",
-        location: "Belgrano",
-        healthIssues: "Ninguno",
-        surgeries: "Vacunado",
-        pictureThumbnailUrl: "https://placekitten.com/402/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Nina",
-        species: "Gato",
-        breed: "Sphynx",
-        age: "1 año",
-        size: "Pequeño",
-        location: "Palermo",
-        healthIssues: "Ninguno",
-        surgeries: "Vacunada",
-        pictureThumbnailUrl: "https://placekitten.com/403/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Lily",
-        species: "Gato",
-        breed: "British Shorthair",
-        age: "4 años",
-        size: "Mediano",
-        location: "San Isidro",
-        healthIssues: "Ninguno",
-        surgeries: "Vacunada",
-        pictureThumbnailUrl: "https://placekitten.com/404/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Chester",
-        species: "Gato",
-        breed: "Exótico de Pelo Corto",
-        age: "6 meses",
-        size: "Mediano",
-        location: "Caballito",
-        healthIssues: "Ninguno",
-        surgeries: "Vacunado",
-        pictureThumbnailUrl: "https://placekitten.com/405/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Toby",
-        species: "Gato",
-        breed: "Abisinio",
-        age: "2 años",
-        size: "Mediano",
-        location: "Recoleta",
-        healthIssues: "Ninguno",
-        surgeries: "Vacunado",
-        pictureThumbnailUrl: "https://placekitten.com/406/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Sasha",
-        species: "Gato",
-        breed: "Siamés",
-        age: "3 años",
-        size: "Pequeño",
-        location: "La Plata",
-        healthIssues: "Problemas estomacales",
-        surgeries: "Vacunada",
-        pictureThumbnailUrl: "https://placekitten.com/407/400"
-      }
-    },
-    {
-      attributes: {
-        name: "Peach",
-        species: "Gato",
-        breed: "Siamés",
-        age: "4 años",
-        size: "Mediano",
-        location: "Morón",
-        healthIssues: "Alergia estacional",
-        surgeries: "Vacunada",
-        pictureThumbnailUrl: "https://placekitten.com/408/400"
-      }
+      name: "Milo",
+      breed: "Cocker Spaniel",
+      age: "7 meses",
+      size: "Mediano",
+      location: "Almirante Brown",
+      healthIssues: "Ninguno",
+      surgeries: "Castrado",
+      pictureThumbnailUrl: dogImages[10]
     }
   ];
 }
